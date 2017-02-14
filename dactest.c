345adc2a094c99a0402a3554b3fabed8f7958563
tau <tau@tau.tau>
2017-02-14 17:15:33 +0000

    lalala

--- dactest.c
+++ dactest.c
@@ -16,8 +16,8 @@ int main(int argc, char **argv){
 	}
 
 	bcm2835_spi_setBitOrder(BCM2835_SPI_BIT_ORDER_MSBFIRST);
-	bcm2835_spi_setDataMode(BCM2835_SPI_MODE3);
-	bcm2835_spi_setClockDivider(8192); //Clock freq: 250MHz/divider.
+	bcm2835_spi_setDataMode(BCM2835_SPI_MODE0);
+	bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_65536); //Clock freq: 250MHz/divider.
 	bcm2835_spi_setChipSelectPolarity(BCM2835_SPI_CS1, LOW);
 	bcm2835_spi_chipSelect(BCM2835_SPI_CS1);
 
@@ -40,7 +40,11 @@ int main(int argc, char **argv){
 	}
 
 	while (1){
-		bcm2835_spi_writenb(&tbuf[0], len);
+		bcm2835_spi_writenb(&tbuf[0], len-2);
 		bcm2835_delayMicroseconds(10);
 	}
+
+	bcm2835_spi_end();
+        bcm2835_close();
+        return 0;
 }	
