3f13b8b4fa2ec7639135c9a56957d66f9ca869aa
tau <tau@tau.tau>
2017-03-28 10:14:48 +0000

    blabla

--- ed_dac.c
+++ ed_dac.c
@@ -3,8 +3,30 @@
 #include <bcm2835.h>
 #include <stdio.h>
 #include <time.h>
+#include <math.h>
+#include <stdint.h>
+
+uint8_t sineBuf[1024];
+void initSineBuf(){
+	uint16_t i;
+	
+	for(i = 0; i < 1024; i++){
+		sineBuf[i] = (sin( ((double)i/1024) *(2*3.141592) ) + 1) * 2048;
+	}
+}
 
 int main(int argc, char **argv){
+
+	initSineBuf();
+	uint16_t i;
+	for(i = 0; i < 1024; i++){
+		printf("%d\t", sineBuf[i]);
+		if(i % 10 == 0){
+			printf("\n");
+		}
+	}
+	for(;;);
+
 	// Initialize the SPI and start a SPI session:
 	if (!bcm2835_init()){
 		printf("bcm2835_init failed. Are you running as root??\n");
@@ -17,26 +39,31 @@ int main(int argc, char **argv){
 
 	bcm2835_spi_setBitOrder(BCM2835_SPI_BIT_ORDER_MSBFIRST);
 	bcm2835_spi_setDataMode(BCM2835_SPI_MODE0);
-	bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_65536); //Clock freq: 250MHz/divider.
+	bcm2835_spi_setClockDivider(100); //Clock freq: 250MHz/divider.
 	bcm2835_spi_setChipSelectPolarity(BCM2835_SPI_CS1, LOW);
 	bcm2835_spi_chipSelect(BCM2835_SPI_CS1);
+
+	uint16_t bufIndex = 0;
+	while(1){
+		bcm2835_spi_writenb(&sineBuf[bufIndex += 2], 2);
+
+	}
+
 
 	uint32_t len = 2;
 	char tbuf[len];
 
 	while(1){
-	 	tbuf[0] = 0x0F
-	 	tbuf[1] = 0xFF
+	 	tbuf[0] = 0x0F;
+	 	tbuf[1] = 0xFF;
 		bcm2835_spi_writenb(&tbuf[0], len);
-		bcm2835_delayMilliseconds(500);
-		tbuf[0] = 0x05
-	 	tbuf[1] = 0x55
+		tbuf[0] = 0x05;
+	 	tbuf[1] = 0x55;
 		bcm2835_spi_writenb(&tbuf[0], len);
-		bcm2835_delayMilliseconds(500);
-		tbuf[0] = 0x00
-	 	tbuf[1] = 0x00
+		tbuf[0] = 0x00;
+	 	tbuf[1] = 0x00;
 		bcm2835_spi_writenb(&tbuf[0], len);
-		bcm2835_delayMilliseconds(500);
+		bcm2835_delayMicroseconds(90);
 	}
 
 
